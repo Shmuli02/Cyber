4660e70063ed5c4d67393d6a3d16f8e1cff1098c
Shmuli02 <samuli.kaleva@gmail.com>
2021-12-21 09:34:23 +0000

    fix PagesConfig problem

--- server/config/settings.py
+++ server/config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'server.pages'
+	'src.pages'
 ]
 
 MIDDLEWARE = [
--- server/config/urls.py
+++ server/config/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 	path('login/', LoginView.as_view(template_name='pages/login.html')),
 	path('logout/', LogoutView.as_view(next_page='/')),
-	path('', include('server.pages.urls'))
+	path('', include('src.pages.urls'))
 ]
